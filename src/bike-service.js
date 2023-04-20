13528ead0adb4e7965d3438ad1f4ef44dce7c184
MarcusKyung <Kyungmj@gmail.com>
2023-04-20 13:11:33 -0700

    add code JS code for bike manufacturer
    Co-Authored: Eusebie Siebenberg <siebenee@gmail.com>

--- src/bike-service.js
+++ src/bike-service.js
@@ -1,6 +1,6 @@
 export default class BikeService{
-  static getBike(bikeLimit, bikeLocation, bikeColor, bikeLocationDistance) {
-    return fetch(`https://bikeindex.org/api/v3/search?page=1&per_page=${bikeLimit}&colors=${bikeColor}&location=${bikeLocation}&distance=${bikeLocationDistance}&stolenness=proximity`)
+  static getBike(bikeLimit, bikeManufacturer, bikeLocation, bikeColor, bikeLocationDistance) {
+    return fetch(`https://bikeindex.org/api/v3/search?page=1&per_page=${bikeLimit}&manufacturer=${bikeManufacturer}&colors=${bikeColor}&location=${bikeLocation}&distance=${bikeLocationDistance}&stolenness=proximity`)
       .then(function(response) {
         if (!response.ok) {
           const errorMessage = `${response.status} ${response.statusText}`;
--- src/index.js
+++ src/index.js
@@ -5,30 +5,31 @@ import BikeService from './bike-service';
 
 // Business Logic
 
-function getBike(bikeLimit, bikeLocation, bikeColor, bikeLocationDistance) {
-  BikeService.getBike(bikeLimit, bikeLocation, bikeColor, bikeLocationDistance) 
+function getBike(bikeLimit, bikeManufacturer, bikeLocation, bikeColor, bikeLocationDistance) {
+  BikeService.getBike(bikeLimit, bikeManufacturer, bikeLocation, bikeColor, bikeLocationDistance) 
     .then(function(response) {
       if (response.bikes) {
-        printElements(response, bikeLimit, bikeLocation, bikeColor, bikeLocationDistance);
+        printElements(response, bikeLimit, bikeManufacturer, bikeLocation, bikeColor, bikeLocationDistance);
       } else {
-        printError(response, bikeLocation, bikeColor, bikeLocationDistance);
+        printError(response, bikeLocation, bikeManufacturer, bikeColor, bikeLocationDistance);
       }
     });
 }
 
 // UI Logic
 
-function printError(error, bikeLocation, bikeColor, bikeLocationDistance) {
-  document.querySelector('#errorMessage').innerText = `There was an error accessing data for the ${bikeColor} bikes within ${bikeLocationDistance} miles of ${bikeLocation}: ${error}.`;
+function printError(error, bikeLocation, bikeManufacturer, bikeColor, bikeLocationDistance) {
+  document.querySelector('#errorMessage').innerText = `There was an error accessing data for the ${bikeColor} ${bikeManufacturer} bikes within ${bikeLocationDistance} miles of ${bikeLocation}: ${error}.`;
 }
 
 function printElements(response, bikeLimit) {
   for (let i = 0; i < bikeLimit; i++) {
     const bikeDetails = document.createElement("ul");
     bikeDetails.classList.add("bike-details");
-
     const bikeTitle = document.createElement("li");
     bikeDetails.append(bikeTitle);
+    const bikeManufacturer = document.createElement("li");
+    bikeDetails.append(bikeManufacturer);
     const bikeColor = document.createElement("li");
     bikeDetails.append(bikeColor);
     const bikeStolenTime = document.createElement("li");
@@ -38,12 +39,13 @@ function printElements(response, bikeLimit) {
     const bikeWebpage = document.createElement("li");
     bikeDetails.append(bikeWebpage);
     bikeTitle.innerText = "Bike Title: " + `${response.bikes[i].title}`;
+    bikeManufacturer.innerText = "Manufacturer: " + `${response.bikes[i].manufacturer_name}`;
     bikeColor.innerText = "Frame Color(s) : " + `${response.bikes[i].frame_colors}`;
     bikeLocation.innerText =  "Stolen Location : " + `${response.bikes[i].stolen_location}`;
 
     const timestamp = `${response.bikes[i].date_stolen}`;
     const date = new Date(timestamp * 1000);
-    const month = date.getMonth() +  1;
+    const month = date.getMonth() + 1;
     const day = date.getDate();
     const year = date.getFullYear();
     const dateStolenOutput = `${month}/${day}/${year}`;
@@ -52,27 +54,29 @@ function printElements(response, bikeLimit) {
     const bikeWebpageLink = document.createElement("a");
     bikeWebpageLink.href = `${response.bikes[i].url}`;
     bikeWebpageLink.innerText = "Stolen Bike Listing";
+    bikeWebpageLink.target = "blank";
     bikeWebpage.append(bikeWebpageLink);
     const result = document.querySelector('#resultsDiv');
     result.append(bikeDetails);
   }
 }
 
-
 function handleFormSubmission(event) {
   event.preventDefault();
   const bikeLimit = document.querySelector('#bikeLimit').value;
+  const bikeManufacturer = document.querySelector('#bikeManufacturer').value;
   const bikeLocation = document.querySelector('#bikeLocation').value;
   const bikeColor = document.querySelector('#bikeColor').value;
   const bikeLocationDistance = document.querySelector('#bikeLocationDistance').value;
   document.querySelector('#bikeLimit').value = null;
+  document.querySelector('#bikeManufacturer').value = null;
   document.querySelector('#bikeLocation').value = null;
   document.querySelector('#bikeColor').value = null;
   document.querySelector('#bikeLocationDistance').value = null;
-  getBike(bikeLimit, bikeLocation, bikeColor, bikeLocationDistance);
+  getBike(bikeLimit, bikeManufacturer, bikeLocation, bikeColor, bikeLocationDistance);
+  
 }
 
-
 window.addEventListener("load", function() {
-  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+  document.getElementById("bike-selection").addEventListener("submit", handleFormSubmission);
 });
